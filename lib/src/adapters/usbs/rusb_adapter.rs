948fb1b154e6d73f807b80abe935cefbf86e4085
thullyDev <thully.developer@gmail.com>
2026-09-13 02:28:38 +0200

    #13 ignoring discovers devices test for ci/cd pipeline

--- lib/src/adapters/usbs/rusb_adapter.rs
+++ lib/src/adapters/usbs/rusb_adapter.rs
@@ -209,6 +209,7 @@ mod tests {
     }
 
     #[test]
+    #[ignore = "requires physical USB device"]
     fn creates_connection_for_discovered_device() {
         let adapter = RusbAdapter::new();
 
@@ -224,6 +225,7 @@ mod tests {
     }
 
     #[test]
+    #[ignore = "requires physical USB device"]
     fn connection_fails_when_not_open() {
         let adapter = RusbAdapter::new();
 
@@ -243,6 +245,7 @@ mod tests {
     }
 
     #[test]
+    #[ignore = "requires physical USB device"]
     fn read_fails_when_not_open() {
         let adapter = RusbAdapter::new();
 
@@ -262,6 +265,7 @@ mod tests {
     }
 
     #[test]
+    #[ignore = "requires physical USB device"]
     fn close_closes_connection() {
         let adapter = RusbAdapter::new();
 
